0b547b8f7cbf2d801cf38050db9434bbc88bb2a4
pmkjkr <pmkjkr@naver.com>
2017-07-05 14:36:05 +0900

    Initial Commit

---
+++ Boo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boo2
+//
+//  Created by pmkjkr on 2017. 7. 5..
+//  Copyright © 2017년 univ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
